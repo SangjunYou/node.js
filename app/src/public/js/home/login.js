e6f12fdad154568caff415df45afb89f673f511a
SangjunYou <fbtkdwns12@gmail.com>
2021-10-08 15:50:05 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -20,5 +20,14 @@ function login() {
         body: JSON.stringify(req),
     })
     .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+        if (res.success) {
+            location.href = "/";
+        } else {
+            alert(res.mag);
+        }
+    })
+    .catch((err) => {
+        console.error("로그인중 에러 발생");
+    });
 };
