509abdce5cb2982eff6e0f4e07738f355a013b28
SangjunYou <fbtkdwns12@gmail.com>
2021-10-08 15:33:36 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 const output = {
     home: (req, res) => {
         res.render("home/index")
@@ -9,10 +11,30 @@ const output = {
     },
 };
 
+const users = {
+    id : ["home5635", "유상준", "정현진"],
+    psword : ["12344", "123", "black"],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
-    },
+        const id = req.body.id,
+          psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    }
 }
 
 
